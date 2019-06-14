29c4c93fc369d44e10a341ff94cd8f966adc271f
Voloshyna Jelena <voloshynajelena@gmail.com>
2019-06-14 14:06:10 +0300

    add parser for data

--- src/service.js
+++ src/service.js
@@ -2,32 +2,73 @@ import fetch from 'cross-fetch';
 
 
 class GitSearch {
- createEvents (){
-   let btnSearch = document.querySelector('.btnSearch')
-   btnSearch.addEventListener('click',e=>{
-    
-    this.getData( this.getInputData() )
-  },false)
- }
- getInputData(){
-  let inpSearch = document.querySelector('.inpSearch')
-  
+
+  createEvents() {
+    let btnSearch = document.querySelector('.btnSearch')
+    btnSearch.addEventListener('click', async e => {
+
+      console.log("---->", "createEvents()")
+      const data = await this.getData(this.getInputData())
+      this.parseData(data)
+    }, false)
+  }
+
+  getInputData() {
+    let inpSearch = document.querySelector('.inpSearch')
+    console.log("---->", "getInputData()")
     return inpSearch.value
- }
- getUrl (srsearch='es6', rclimit=6){
+  }
+
+  /**
+   * 
+   * @param {string} srsearch 
+   * @param {number} rclimit 
+   * @returns {string}
+   */
+  getUrl(srsearch = 'es6', rclimit = 6) {
+
+    console.log("---->", "getUrl()")
     const urlApi = "https://api.github.com/search/repositories?"
-    let per_page=10,
-        url=`${urlApi}q=${srsearch}&page=${rclimit}&per_page=${per_page}`
+    let per_page = 10,
+      url = `${urlApi}q=${srsearch}&page=${rclimit}&per_page=${per_page}`
     return url
-}
+  }
+  /**
+   * 
+   * @param {string} key1 
+   * @param {number} key2
+   * @returns {promise} 
+   */
+  getData(key1, key2) {
+    console.log("---->", "getData()")
+    return fetch(this.getUrl(key1, key2))
+      .then(function (response) {
+        return response.json();
+      })
+  }
+  /**
+   * 
+   * @param {obj} sourseData
+   * @returns {Array} 
+   */
+  parseData(sourseData) {
+    console.log("----> parseData()")
 
- getData(key1,key2){
-   
-   return fetch(this.getUrl(key1,key2))
-  .then(function(response) {
-    return response.json();
-  })
-}
 
+    let parsedData = sourseData.items.map(function (item) {
+      return {
+        title: item.name,
+        url: item.git_url,
+        description: item.description
+      }
+    })
+console.log(parsedData)
+return parsedData
+  }
+
+  searchResult() {
+    let inpResult = document.querySelector('.inpResult')
+    inpResult.innerHTML
+  }
 }
 export default GitSearch
